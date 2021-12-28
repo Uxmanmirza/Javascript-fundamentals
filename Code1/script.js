4b0118d4891a21ddbd7b8742c8789a32f014fa87
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2021-12-28 14:01:45 +0500

    data types assign

--- Code1/script.js
+++ Code1/script.js
@@ -5,3 +5,14 @@
 // console.log(country);
 // console.log(continent);
 // console.log(population);
+
+let island = "Pakistan";
+let population = "2.3 Billions";
+let ccountry = "Pakistan";
+island = true;
+let language;
+
+console.log(island);
+console.log(population);
+console.log(ccountry);
+console.log(language);
