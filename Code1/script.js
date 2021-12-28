cc4ac65860c0069ce1c0f026e000f9246973d7b9
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2021-12-28 13:54:05 +0500

    value and variable assign

---
+++ Code1/script.js
@@ -0,0 +1,7 @@
+// const country = "Pakistan";
+// const continent = "Asia";
+// const population = "2.3 Billion";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
