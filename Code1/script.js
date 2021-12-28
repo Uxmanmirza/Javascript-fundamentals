8d848594269f9f41591369dc1a6b6b0bda1bd2d9
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2021-12-28 14:07:37 +0500

    variables assign

--- Code1/script.js
+++ Code1/script.js
@@ -6,13 +6,22 @@
 // console.log(continent);
 // console.log(population);
 
-let island = "Pakistan";
-let population = "2.3 Billions";
-let ccountry = "Pakistan";
-island = true;
-let language;
+// let island = "Pakistan";
+// let population = "2.3 Billions";
+// let ccountry = "Pakistan";
+// island = true;
+// let language;
 
-console.log(island);
-console.log(population);
-console.log(ccountry);
+// console.log(island);
+// console.log(population);
+// console.log(ccountry);
+// console.log(language);
+
+let language = "Urdu";
+const province = "Punjab";
+language = "English";
+language = "Punjabi";
+// province = "Sindh";
+
 console.log(language);
+console.log(province);
