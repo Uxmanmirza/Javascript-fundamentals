d07ffbba1b2c646439101a23954881b7935696d7
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2021-12-29 14:36:43 +0500

    code 3 challange

--- Code1/Code 2/code 3/script.js
+++ Code1/Code 2/code 3/script.js
@@ -1,11 +1,75 @@
-let numNeighours = Number(
-  prompt("How many neighbour countries does your country have?")
-);
+// let numNeighours = Number(
+//   prompt("How many neighbour countries does your country have?")
+// );
 
-if (numNeighours === 1) {
-  console.log("Only 1 border!");
-} else if (numNeighours > 1) {
-  console.log("more than one border!");
+// if (numNeighours === 1) {
+//   console.log("Only 1 border!");
+// } else if (numNeighours > 1) {
+//   console.log("more than one border!");
+// } else {
+//   console.log("NO border");
+// }
+
+// let pakistanlanguage = "English";
+// let pakistanpopulation = "45000";
+// let pakistanisland = "landscape";
+
+// let language = true;
+// let population = true;
+// let island = false;
+
+// if (language && population && island) {
+//   console.log("Pakistan is perfect land to live for you sara");
+// } else {
+//   console.log("Pakistan is not according to your criteria ");
+// }
+
+// match1
+let Dolphins = 96 + 108 + 89 / 3;
+let Koalas = 88 + 91 + 110 / 3;
+
+console.log(Dolphins);
+console.log(Koalas);
+
+let match1result = Dolphins > Koalas;
+match1result = true;
+
+console.log(match1result);
+
+// Match2
+let DolphinsBonus1 = 97 + 112 + 101 / 3;
+let KoalasBonus1 = 109 + 95 + 123 / 3;
+
+console.log(DolphinsBonus1);
+console.log(KoalasBonus1);
+let match2result = KoalasBonus1 > DolphinsBonus1;
+match2result = true;
+console.log(match2result);
+
+if ((DolphinsBonus1 && KoalasBonus1 > 100, KoalasBonus1 > DolphinsBonus1)) {
+  KoalasBonus1 = true;
+  console.log("Koalas win this match");
 } else {
-  console.log("NO border");
+  console.log("No one win because score is less than 100");
+}
+
+// Match3
+let DolphinsBonus2 = 97 + 112 + 101 / 3;
+let KoalasBonus2 = 109 + 95 + 106 / 3;
+
+console.log(DolphinsBonus2);
+console.log(KoalasBonus2);
+
+if (DolphinsBonus2 && KoalasBonus2 > 100) {
+  console.log("This match is draw because score is greater than the 100");
+}
+
+// FinalTrophywinningtime
+
+let winnerteam = Number(prompt("Which team win the match?"));
+
+if ((winnerteam = Dolphins || Koalas)) {
+  console.log("This series is equal :)");
+} else {
+  console.log("please enter the correct name of team");
 }
